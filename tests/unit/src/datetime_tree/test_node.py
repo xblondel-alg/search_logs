c855699fa530df2449f78d925e3c2e277e9edf9d
xblondel-alg <blondel.xavier+alg@gmail.com>
2021-06-21 18:22:00 +0200

    test for multiple minutes interval

--- tests/unit/src/datetime_tree/test_node.py
+++ tests/unit/src/datetime_tree/test_node.py
@@ -113,3 +113,42 @@ class TestNode:
         actual = list(actual_generator)
 
         assert expected == actual
+
+    def test_find_multiple_minutes_interval(self, tree: RootNode[str]) -> None:
+        """
+        Should find all data in an interval containing multiple minutes
+        """
+        # we fill 3 consecutive minutes, and we look for the middle one
+        key1 = datetime(2015, 10, 1, 12, 22)
+        value1_1 = "test1_1"
+        value1_2 = "test1_2"
+        tree.add_value(key1, value1_1)
+        tree.add_value(key1, value1_2)
+        key2 = datetime(2015, 10, 1, 12, 23)
+        value2_1 = "test2_1"
+        value2_2 = "test2_2"
+        tree.add_value(key2, value2_1)
+        tree.add_value(key2, value2_2)
+        key3 = datetime(2015, 10, 1, 12, 24)
+        value3_1 = "test3_1"
+        value3_2 = "test3_2"
+        tree.add_value(key3, value3_1)
+        tree.add_value(key3, value3_2)
+        key4 = datetime(2015, 10, 1, 12, 25)
+        value4_1 = "test4_1"
+        value4_2 = "test4_2"
+        tree.add_value(key4, value4_1)
+        tree.add_value(key4, value4_2)
+
+        # we expect to find the values for key2 and key3, but not for key4 (as the interval is open)
+        expected = [value2_1, value2_2, value3_1, value3_2]
+        actual_generator = tree.get_values_for_interval(
+            (
+                key2,
+                key4,
+            )
+        )
+        # resolve the generator
+        actual = list(actual_generator)
+
+        assert expected == actual
